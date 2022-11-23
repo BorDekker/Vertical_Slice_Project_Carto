e1f493712d8a53551837b60f652d45773d5cc9ef
Bor Dekker <32562@ma-web.nl>
2022-11-23 11:54:03 +0100

    23th November 2022 11:53

---
+++ BO-Carto/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float movementSpeed;
+
+    public Rigidbody2D rb2D;
+
+    private Vector2 movementDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessingInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessingInputs()
+    {
+        float movementX = Input.GetAxisRaw("Horizontal");
+        float movementY = Input.GetAxisRaw("Vertical");
+
+        movementDirection = new Vector2(movementX, movementY).normalized;
+    }
+
+    void Move()
+    {
+        rb2D.velocity = new Vector2(movementDirection.x * movementSpeed, movementDirection.y * movementSpeed);
+    }
+}
