34da03b90c6bc0769a99baca3c67a6b92a3309db
Bor Dekker <32562@ma-web.nl>
2022-11-23 12:50:03 +0100

    23th November 2022 12:49

--- BO-Carto/Assets/Scripts/BehaviourAI.cs
+++ BO-Carto/Assets/Scripts/BehaviourAI.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class BehaviourAI : MonoBehaviour
 {
-    public Vector3 velocity = new Vector3();
-
     public GameObject[] waypointsKid;
-    int waypointsKidIndex = 0;
+    public int waypointsKidIndex = 0;
 
     [SerializeField]
     float moveSpeedKid = 2f;
@@ -15,42 +13,19 @@ public class BehaviourAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = waypointsKid[waypointsKidIndex].transform.position;
+        waypointsKidIndex = Random.Range(0, 12);
 
-        /*
-        velocity.x = Random.Range(-5, 5);
-        velocity.y = Random.Range(-5, 5);
-        */
+        transform.position = waypointsKid[waypointsKidIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        if(GameObject.FindGameObjectWithTag("Kid"))
-        {
-            transform.position += velocity * Time.deltaTime;
-
-            if(transform.position.x > 3)
-            {
-                velocity.x = -velocity.x;
-            }
-            if(transform.position.x < -3)
-            {
-                velocity.x = -velocity.x;
-            }
-
-            if(transform.position.y > 3)
-            {
-                velocity.y = -velocity.y;
-            }
-            if(transform.position.y < -3)
-            {
-                velocity.y = -velocity.y;
-            }
-        }
-        */
-
         transform.position = Vector3.MoveTowards(transform.position, waypointsKid[waypointsKidIndex].transform.position, moveSpeedKid * Time.deltaTime);
+
+        if(transform.position == waypointsKid[waypointsKidIndex].transform.position)
+        {
+            waypointsKidIndex = Random.Range(0, 12);
+        }
     }
 }
