bfd3ea10710c0e84d81b382595abb890bed1fd4a
mo <mosawer.asg@outlook.com>
2022-11-30 15:01:43 +0100

    Uitzoomen map partially done

--- BO-Carto/Assets/Scripts/MapEffects.cs
+++ BO-Carto/Assets/Scripts/MapEffects.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class MapEffects : MonoBehaviour
 {
-    public float variableToChangeX, variableToChangeY;
+    public float ZoomX, ZoomY;
     public float changePerSecondX, changePerSecondY;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    // Dit wil ik gebruiken om een max zoomuit te setten
+    //public float MaxZoomX, MaxZoomY;
 
     // Update is called once per frame
     void Update()
     {
-        transform.localScale = new Vector3(variableToChangeX, variableToChangeY, 1);
-        //variableToChangeX += changePerSecond * Time.deltaTime;
-        //variableToChangeY += changePerSecond * Time.deltaTime;
-        if (Input.GetKey(KeyCode.M))
+        transform.localScale = new Vector3(ZoomX, ZoomY, 1);
+        if (Input.GetKey(KeyCode.M) && ZoomX >= 0f)
         {
-            variableToChangeX += changePerSecondX * Time.deltaTime;
-            variableToChangeY += changePerSecondY * Time.deltaTime;
+            ZoomOutX();
         }
+
+        if (Input.GetKey(KeyCode.M) && ZoomY >= 0f)
+        {
+            ZoomOutY();
+        }
+    }
+
+    void ZoomOutX()
+    {
+        ZoomX += changePerSecondY * Time.deltaTime;
+    }
+    void ZoomOutY()
+    {
+        ZoomY += changePerSecondY * Time.deltaTime;
     }
 }
