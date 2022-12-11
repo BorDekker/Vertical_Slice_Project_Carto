1dd903ade5d3679691bfb46aa82da5a265fc204e
mo <mosawer.asg@outlook.com>
2022-12-11 21:42:58 +0100

    Movement Fixed

---
+++ BO-Carto/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public Vector3 velocity = new Vector3();
+    float Movementspeed = 3f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+            transform.position += transform.right * Movementspeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+            transform.position += -transform.right * Movementspeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W))
+            transform.position += transform.forward * Movementspeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.S))
+            transform.position += -transform.forward * Movementspeed * Time.deltaTime;
+
+    }
+}
