391d52f6cf5401d784125ec4ade76b40f3233775
Bor Dekker <32562@ma-web.nl>
2022-12-17 22:39:39 +0100

    17 December 2022

--- BO-Carto/Assets/Scripts/PlayerBehaviour.cs
+++ BO-Carto/Assets/Scripts/PlayerBehaviour.cs
@@ -10,12 +10,6 @@ public class PlayerBehaviour : MonoBehaviour
 
     private Vector2 movementDirection;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
