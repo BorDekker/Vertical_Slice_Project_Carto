526d84fc2ee444a34fffabedb9e3d4a6b12bdf8d
mo <mosawer.asg@outlook.com>
2022-12-19 10:50:39 +0100

    Finalizing zoom in and out main camera

--- BO-Carto/Assets/Scripts/Map_State.cs
+++ BO-Carto/Assets/Scripts/Map_State.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Map_State : MonoBehaviour
 {
+    public float xPosition, yPosition, zPosition;
+    public float changePerSecondY;
+    public float changePerSecondZ;
+
 
     public GameObject image;
+    public GameObject Camera;
     public enum State
     {
         OPEN,
@@ -14,13 +19,26 @@ public class Map_State : MonoBehaviour
 
     public State currentState = State.CLOSED;
 
+    private void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
+        Camera.transform.localPosition = new Vector3(xPosition, yPosition, zPosition);
+
+        Debug.Log("curr" + currentState);
         switch (currentState)
         {
             case State.OPEN:
                 image.SetActive(true);
+                if(yPosition <=43 && zPosition >= -59)
+                {
+                    yPosition += changePerSecondY * Time.deltaTime;
+                    zPosition += changePerSecondZ * Time.deltaTime;
+                }
                 if (Input.GetKeyDown(KeyCode.M))
                 {
                     currentState = State.CLOSED;
@@ -29,6 +47,11 @@ public class Map_State : MonoBehaviour
 
             case State.CLOSED:
                 image.SetActive(false);
+                if (yPosition >= 11.26308f && zPosition <= -27.90452f)
+                {
+                    yPosition -= changePerSecondY * Time.deltaTime;
+                    zPosition -= changePerSecondZ * Time.deltaTime;
+                }
                 if (Input.GetKeyDown(KeyCode.M))
                 {
                     currentState = State.OPEN;
