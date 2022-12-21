3590392cc0e7b67b8419bb70898b5be951b33230
Bor Dekker <32562@ma-web.nl>
2022-12-21 10:45:20 +0100

    fixing merge conflict

--- BO-Carto/Assets/Scripts/Map_State.cs
+++ BO-Carto/Assets/Scripts/Map_State.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Map_State : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup myUIGroup;
+
     public float xPosition, yPosition, zPosition;
     public float changePerSecondY;
     public float changePerSecondZ;
 
+    public float ScalePerSecond;
+
+    public float xScale;
+    public float yScale;
 
     public GameObject image;
     public GameObject Camera;
@@ -28,12 +35,26 @@ public class Map_State : MonoBehaviour
     void Update()
     {
         Camera.transform.localPosition = new Vector3(xPosition, yPosition, zPosition);
+        image.transform.localScale = new Vector3(xScale, yScale, 1);
 
         Debug.Log("curr" + currentState);
         switch (currentState)
         {
             case State.OPEN:
                 image.SetActive(true);
+
+                //Fading in UI
+                if (xScale <= 4.2 && yScale <= 4.2 && xScale >= 1 && yScale >= 1)
+                {
+                    xScale -= ScalePerSecond * Time.deltaTime;
+                    yScale -= ScalePerSecond * Time.deltaTime;
+                }
+                if (myUIGroup.alpha < 1)
+                {
+                    myUIGroup.alpha += Time.deltaTime * 2;
+                }
+
+                //Camera moves away from player
                 if(yPosition <=43 && zPosition >= -59)
                 {
                     yPosition += changePerSecondY * Time.deltaTime;
@@ -46,7 +67,25 @@ public class Map_State : MonoBehaviour
                 break;
 
             case State.CLOSED:
-                image.SetActive(false);
+                //Fading out UI
+                xScale = 4;
+                yScale = 4;
+                //dit werkt nog niet zoals ik wil, raadpleeg docent
+                if (xScale <= 1 && yScale <= 1)
+                {
+                    xScale += ScalePerSecond * Time.deltaTime;
+                    yScale += ScalePerSecond * Time.deltaTime;
+                }
+                if (myUIGroup.alpha >= 0)
+                {
+                    myUIGroup.alpha -= Time.deltaTime * 2;
+                    if(myUIGroup.alpha == 0)
+                    {
+                        image.SetActive(false);
+                    }
+                }
+
+                //Camera Moves towards player
                 if (yPosition >= 11.26308f && zPosition <= -27.90452f)
                 {
                     yPosition -= changePerSecondY * Time.deltaTime;
